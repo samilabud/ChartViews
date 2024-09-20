63036e590d29e8213040255c4cae9b5f67ff32bd
Samil Abud <samilabud@gmail.com>
2024-09-19 22:13:28 -0400

    Fixes replace the deprecated faker.datatype.number with faker.number.int

--- app/chartjs/bubble-chart/page.tsx
+++ app/chartjs/bubble-chart/page.tsx
@@ -27,18 +27,18 @@ const data = {
     {
       label: "Red dataset",
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
-        r: faker.datatype.number({ min: 5, max: 20 }),
+        x: faker.number.int({ min: -100, max: 100 }),
+        y: faker.number.int({ min: -100, max: 100 }),
+        r: faker.number.int({ min: 5, max: 20 }),
       })),
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Blue dataset",
       data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
-        r: faker.datatype.number({ min: 5, max: 20 }),
+        x: faker.number.int({ min: -100, max: 100 }),
+        y: faker.number.int({ min: -100, max: 100 }),
+        r: faker.number.int({ min: 5, max: 20 }),
       })),
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
